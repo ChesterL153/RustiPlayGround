409dad166fa7cede92ca71a75b8660168a2447c7
ChiHo Kwan <45314406+chk703@users.noreply.github.com>
2021-08-27 23:08:33 +0100

    Another improvement

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -7,19 +7,24 @@ fn main() {
     println!("Please choose a number from 1 to 10 ...");
 
     let secret = rand::thread_rng().gen_range(1..11); //gen range is lower bound .. upper bound + 1
-    let mut number = String::new();
 
-    io::stdin()
-        .read_line(&mut number)
-        .expect("Not input correctly");
+    loop{
+        let mut number = String::new();
 
-    let number: u32 = number.trim().parse().expect("Please type a number!");
+        io::stdin()
+            .read_line(&mut number)
+            .expect("Not input correctly");
 
-    match number.cmp(&secret) {
-        Ordering::Less => print!("Too small!"),
-        Ordering::Greater => print!("Too big!"),
-        Ordering::Equal => print!("You win!"),
+        let number: u32 = number.trim().parse().expect("Please type a number!");
+
+        match number.cmp(&secret) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You Win!"); 
+                break;} 
+        }
     }
 
-    println!(" The secret number is: {}", secret);
+    println!("The secret number is: {}", secret);
 }
